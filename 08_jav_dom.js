aa0c631eb22b090dd0f56e4e05e8f3ea3ac21c36
Smaran Rawal <smaranrawal123@gmail.com>
2024-10-17 10:59:24 +0545

    added files

---
+++ 08_jav_dom.js
@@ -0,0 +1,17 @@
+// let heading = document.querySelector("h2");
+// heading.innerText= heading.innerText + " from Code with Smaran Rawal";
+// console.log(heading.innerText);
+
+let adds= document.querySelectorAll(".box");
+
+let idx=1;
+for (add of adds){
+    add.innerText=`creating unique value ${idx}`;
+    idx++;
+}
+// add[0].innerText="creating unique text";
+
+
+// add[1].innerText="creating unique ";
+
+// add[2].innerText="creating ";
