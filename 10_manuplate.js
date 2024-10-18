09700dafb0021c6722cfad55498c29bb2fb224a5
Smaran Rawal <smaranrawal123@gmail.com>
2024-10-18 18:33:34 +0545

    newfiles

---
+++ 10_manuplate.js
@@ -0,0 +1,36 @@
+// let div= document.querySelector("div"); //---ACCESSING THE ELEMENT BEFORE CHANGING
+
+
+//-----------CHANGING OF THE ELEMENT ATTRIBUTE---------------------
+// // div.getAttribute("id");
+// // div.setAttribute("id","box");
+
+//------STYLING WITH IN JS---------------
+
+// div.style.backgroundColor="purple";
+// div.style.fontFamily="arial";
+// div.style.fontSize="30px";
+
+//-------CHANGING THE INNER CONTENT IN DIV---------------------
+// div.innerText="Hello!!!";
+
+//---------CREATING A NEW ELEMENT WITHIN JS--------------//
+let butt= document.createElement("button");
+
+butt.innerText="click!!";
+
+
+let div=document.querySelector("div");
+div.after(butt);//------------DISPLAYING IN SCREEN-----------
+
+let newheading=document.createElement("h1");
+newheading.innerText="Welcome to javascript course";
+
+// let sel=document.querySelector("body");
+// sel.after(newheading);
+ document.querySelector("body").prepend(newheading);
+
+ //---------------DELETING THE ELEMENT-----------------
+
+ let para=document.querySelector("p");
+ para.remove();
---
+++ 13_practice.js
@@ -0,0 +1,23 @@
+
+//---------PRACTICE-1---------------
+// let val=document.createElement("button");
+// val.innerText="click me!!!";
+
+// val.style.backgroundColor="red";
+// val.style.color="white";
+
+// let newheading=document.querySelector("body");
+// newheading.prepend(val);
+
+
+
+//---------------PRACTICE-2-----------------
+
+let newclass=document.querySelector("p");
+//newclass.setAttribute("class","newclass");---this is used to change the class of p element
+newclass.classList.add("newclass");
+
+
+
+
+
